a2f0416fcbcbcb6ab0c064147e8f56d2bd2797da
damian majercak <damian.majercak@gmail.com>
2020-11-24 20:28:39 +0100

    bulk insert

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import json
+import requests
+import re
+import numpy as np
+
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Index
 from elasticsearch_dsl import connections
@@ -16,38 +20,60 @@ def create_elastic_index(index_name: str, number_of_shards: int = 3, number_of_r
 
     return es.indices.create(index='tweets',body=elastic_mapping)
     
+
+def load_data(file_name: str):
+    with open(f'data/{file_name}.json') as f:
+        data = json.load(f)
+
+    return data
+
 def main():
     #print(create_elastic_index('tweets'))
     # print(es.indices.exists('tweets'))
+    for part in ['part_2','part_3', 'part_4', 'part_5', 'part_6', 'part_7']
+        data = load_data(part)
+        
+        iter_data = np.array_split(data, 1000)
+        for index, list_of_data in enumerate(iter_data):
+            inser_data(list_of_data)
+            print(f'inserted {(index+1) * 1000 }')
 
     return 0
 
-def importData(index='tweets',mongoDB):
-    batchSize = 10000
-    bulkString = ""
-    for index,doc in enumerate(data):
-        ind = "{\"index\":{}}"
-        stringData=json.dumps(doc)
-        bulkString = bulkString + ind+"\n"+ stringData + "\n"
-        if index%batchSize == 0 and index!=0:
-            response = requests.post(f"http://localhost:9200/{index}/_bulk", data = bulkString.encode("UTF-8"), headers = {"Content-Type":"application/json; charset=utf-8"})
-            bulkString = ""
-            print("odoslanych ",index,response.text)
-    response = requests.post(f"http://localhost:9200/{index}/_bulk", data = bulkString.encode("UTF-8"), headers = {"Content-Type":"application/json; charset=utf-8"})
-    bulkString = ""
-    print("odoslanych ",index,response)
 
-def inser_data(data: list):
+
+
+
+def inser_data(data: list, index='tweets'):
     bulk_string = ''
-    for index,doc in enumerate(data):
-        action_string = f'{{\"index\":{{"_id":{doc["id"]} }}}}'
-        document_string = json.dumps(doc)
-        bulk_string = bulk_string + action_string+"\n"+ document_string + "\n"
+    for doc in data:
+        doc_string = doc['data']
+        doc_id = re.match('^{"id": "[0-9]+', doc_string).__getitem__(0)[8:]
+        action_string = f'{{\"index\":{{"_id": "{doc_id}" }}}}'
+        bulk_string = bulk_string + action_string+"\n"+ doc_string + "\n"
     
-    response = requests.post(f"http://localhost:9200/{index}/_bulk",
-                                data = bulk_string, 
-                                headers = {"Content-Type":"application/json"})
+    response = requests.post(f"http://192.168.0.101:9200/{index}/_bulk",
+                                data = bulk_string.encode("UTF-8"), 
+                                headers = {"Content-Type":"application/json; charset=utf-8"})
+    print(response.status_code)
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
+
+
+
+# def importData(index='tweets',mongoDB):
+#     batchSize = 10000
+#     bulkString = ""
+#     for index,doc in enumerate(data):
+#         ind = "{\"index\":{}}"
+#         stringData=json.dumps(doc)
+#         bulkString = bulkString + ind+"\n"+ stringData + "\n"
+#         if index%batchSize == 0 and index!=0:
+#             response = requests.post(f"http://localhost:9200/{index}/_bulk", data = bulkString.encode("UTF-8"), headers = {"Content-Type":"application/json; charset=utf-8"})
+#             bulkString = ""
+#             print("odoslanych ",index,response.text)
+#     response = requests.post(f"http://localhost:9200/{index}/_bulk", data = bulkString.encode("UTF-8"), headers = {"Content-Type":"application/json; charset=utf-8"})
+#     bulkString = ""
+#     print("odoslanych ",index,response)
